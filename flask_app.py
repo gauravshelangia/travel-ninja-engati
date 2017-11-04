2699903bce1acb328ea3620c56199f46b80191a4
gaurav <gauravshelangia@gmail.com>
2017-11-05 00:42:18 +0530

    requests added

--- flask_app.py
+++ flask_app.py
@@ -554,4 +554,4 @@ def get_quiz(category,level):
     return json.dumps(result)
 
 if __name__ == "__main__":
-    app.run(host='localhost')
+    app.run(host='http://www.travel-ninja-engati.herokuapp.com')
